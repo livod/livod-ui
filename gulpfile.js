54a23b6cfe9c5533e68223b992596de6cee721e1
i_liusiyuan <i_liusiyuan@cvte.com>
2021-02-02 14:39:44 +0800

    chore: 更新gulp配置

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,24 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 var merge = require("merge2");
-
-gulp.task("default", function () {
-  const tsResult = tsProject.src().pipe(tsProject());
+var less = require("gulp-less");
+var replace = require("gulp-replace");
+gulp.task("compileTs", function () {
+  const tsResult = tsProject
+    .src()
+    .pipe(replace("index.less", "index.css"))
+    .pipe(tsProject());
   return merge([
-    tsResult.dts.pipe(gulp.dest("esm/@types")),
+    tsResult.dts.pipe(gulp.dest("esm")),
     tsResult.js.pipe(gulp.dest("esm")),
   ]);
 });
+
+gulp.task("compileLess", function () {
+  return gulp
+    .src("src/**/*.less") //该任务针对的文件，你需要编译的文件
+    .pipe(less()) //该任务调用的模块
+    .pipe(gulp.dest("esm")); //将会在css下生成index.css
+});
+
+gulp.task("default", gulp.series("compileTs", "compileLess"));
