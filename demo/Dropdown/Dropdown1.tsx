23f5ec0f1db9fcd0006f6102175c75303eae3402
i_liusiyuan <i_liusiyuan@cvte.com>
2021-02-07 09:33:26 +0800

    demo: 添加了Dropdown1的demo

---
+++ demo/Dropdown/Dropdown1.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Dropdown, DropdownMenu as Menu } from "../../src";
+export default () => {
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://www.alipay.com/"
+        >
+          1st menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://www.taobao.com/"
+        >
+          2nd menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://www.tmall.com/"
+        >
+          3rd menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item danger>a danger item</Menu.Item>
+    </Menu>
+  );
+  return (
+    <Dropdown overlay={menu}>
+      <a
+        className="livod-dropdown-link"
+        href="#"
+        onClick={(e) => e.preventDefault()}
+      >
+        Hover me
+      </a>
+    </Dropdown>
+  );
+};
