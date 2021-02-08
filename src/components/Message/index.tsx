7b1e81cdcd5857ae0ea4aac128a086c3a390db37
i_liusiyuan <i_liusiyuan@cvte.com>
2021-02-08 11:49:28 +0800

    feat: message组件完成

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -4,14 +4,19 @@ import { Modal } from "react-overlays";
 import SwitchIcon from "./SwitchIcon";
 import "./style/index.less";
 
-export type MessageType = "info" | "error" | "warning" | "success";
+export type MessageType = "info" | "error" | "warning" | "success" | "loading";
+
+export interface MessageReturnType extends PromiseLike<any> {
+  (): void;
+}
 
 export type Message<T extends string> = {
-  [p in T]: (msg: string, timeout?: number) => void;
-} & {
-  loading: (msg: string, timeout?: number, key?: any) => () => void;
+  [p in T]: (
+    msg: string | object,
+    timeout?: number,
+    key?: any
+  ) => MessageReturnType;
 };
-
 // 包裹所有的message
 let container: any = null;
 
@@ -20,7 +25,7 @@ let message: Message<MessageType> = {} as Message<MessageType>;
 let messageTypeArr: MessageType[] = ["info", "error", "warning", "success"];
 
 const OriginMessage: React.FC<any> = React.forwardRef(
-  ({ timeout, tobeRemovedNode, type, msg }, ref) => {
+  ({ timeout, tobeRemovedNode, type, msg, className, style }, ref) => {
     const [show, setShow] = useState(true);
     /* 原生DOM操作实现清除节点 */
     const clearNode = () => {
@@ -61,7 +66,8 @@ const OriginMessage: React.FC<any> = React.forwardRef(
         aria-labelledby="modal-label"
         container={container}
         autoFocus={false}
-        className="livod-message"
+        className={"livod-message " + className}
+        style={style}
       >
         <div>
           <span className="livod-message-icon">{SwitchIcon(type)}</span>
@@ -73,6 +79,12 @@ const OriginMessage: React.FC<any> = React.forwardRef(
 );
 
 const showMessage = (type, msg = "", timeout = 3) => {
+  // 对第二个参数进行纠正
+  let config = null;
+  if (Object.prototype.toString.call(msg) === "[object Object]") {
+    config = msg;
+    msg = config.content;
+  }
   if (!container) {
     container = document.createElement("div");
     container.className = "livod-message-container";
@@ -85,14 +97,32 @@ const showMessage = (type, msg = "", timeout = 3) => {
       timeout={timeout * 1000}
       type={type}
       msg={msg}
+      style={config && config.style}
+      className={config && config.className}
       tobeRemovedNode={tobeRenderedNode}
     ></OriginMessage>,
     tobeRenderedNode
   );
+  return new Promise((resolve, reject) => {
+    try {
+      setTimeout(() => {
+        resolve(undefined);
+      }, timeout * 1000);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
 let loadingHelper = new Map();
-const LoadingMessage = ({ msg = "", timeout = 3, tobeRemovedNode, _key }) => {
+const LoadingMessage = ({
+  msg = "",
+  timeout = 3,
+  tobeRemovedNode,
+  _key,
+  style,
+  className,
+}) => {
   const ref = useRef(null);
   useEffect(() => {
     loadingHelper.set(_key, ref);
@@ -104,10 +134,19 @@ const LoadingMessage = ({ msg = "", timeout = 3, tobeRemovedNode, _key }) => {
       msg={msg}
       tobeRemovedNode={tobeRemovedNode}
       ref={ref}
+      style={style}
+      className={className}
     ></OriginMessage>
   );
 };
 const showLoadingMessage = (msg = "", timeout, key) => {
+  // 对第一个参数进行纠正
+  let config = null;
+  if (Object.prototype.toString.call(msg) === "[object Object]") {
+    config = msg;
+    msg = config.content;
+  }
+
   if (!container) {
     container = document.createElement("div");
     container.className = "livod-message-container";
@@ -122,21 +161,39 @@ const showLoadingMessage = (msg = "", timeout, key) => {
       msg={msg}
       tobeRemovedNode={tobeRenderedNode}
       _key={key}
+      style={config && config.style}
+      className={config && config.className}
     ></LoadingMessage>,
     tobeRenderedNode
   );
 };
 // loading组件判断两种情况，如果传入的timeout为空或者为0，则返回一个可取消的函数。
 // 其它情况则返回一个promise对象。
-message.loading = (msg, timeout = 0, key = {}) => {
+message.loading = ((msg, timeout = 0, key = {}) => {
   showLoadingMessage(msg, timeout, key);
+  if (timeout !== 0) {
+    return new Promise((resolve, reject) => {
+      try {
+        setTimeout(() => {
+          resolve(undefined);
+          if (loadingHelper.has(key)) {
+            loadingHelper.get(key).current.destroy();
+          }
+          loadingHelper.delete(key);
+        }, timeout * 1000);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
   return () => {
     if (loadingHelper.has(key)) {
       loadingHelper.get(key).current.destroy();
     }
     loadingHelper.delete(key);
   };
-};
+}) as (msg: string, timeout?: number, key?: any) => MessageReturnType;
 
 messageTypeArr.forEach((v) => {
   message[v] = showMessage.bind(null, v);
