5e65043ba16b67a113768a17580102c00557efd5
i_liusiyuan <i_liusiyuan@cvte.com>
2021-02-18 16:02:14 +0800

    remove Menu Component

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import Modal from "./components/Modal";
 import Button from "./components/Button";
 import Dropdown from "./components/Dropdown";
-import Menu from "./components/Menu";
 import message from "./components/Message";
 import DropdownMenu from "./components/Dropdown/DropdownMenu";
 import Input from "./components/Input";
 import "./style/index.less";
 
-export { Modal, Button, Dropdown, Menu, DropdownMenu, message, Input };
+export { Modal, Button, Dropdown, DropdownMenu, message, Input };
