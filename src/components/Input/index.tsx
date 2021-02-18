7f4e2558c47d5dbcfb102ed3bcee333740895915
i_liusiyuan <i_liusiyuan@cvte.com>
2021-02-18 14:39:20 +0800

    feat: Input组件添加error样式

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useMemo } from "react";
 import "./style/index.less";
 export interface OriginInputProps {
   placeholder?: string;
   className?: string;
   value?: string;
+  hasError?: boolean;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const OriginInput: React.FC<OriginInputProps> = ({ className, ...rest }) => {
+const OriginInput: React.FC<OriginInputProps> = ({
+  className = "",
+  hasError,
+  ...rest
+}) => {
+  const classname = useMemo(() => {
+    if (className) {
+      className = " " + className;
+    }
+    if (hasError) {
+      className += " has-error";
+    }
+    return "livod-input" + className;
+  }, [className, hasError]);
   return (
     <>
-      <input
-        type="text"
-        className={"livod-input" + (className ? " " + className : "")}
-        {...rest}
-      />
+      <input type="text" className={classname} {...rest} />
     </>
   );
 };
