510930e7817290b1f6d6375d9e64c1d269a1e6c2
i_liusiyuan <i_liusiyuan@cvte.com>
2021-02-19 10:54:11 +0800

    feat: 添加message具名导出

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import Modal from "./components/Modal";
 import Button from "./components/Button";
 import Dropdown from "./components/Dropdown";
-import Message from "./components/Message";
+import message from "./components/Message";
 import DropdownMenu from "./components/Dropdown/DropdownMenu";
 import Input from "./components/Input";
 import Table from "./components/Table";
 import "./style/index.less";
 
-export { Modal, Button, Dropdown, DropdownMenu, Message, Input, Table };
+const Message = message;
+
+export {
+  Modal,
+  Button,
+  Dropdown,
+  DropdownMenu,
+  Message,
+  message,
+  Input,
+  Table,
+};
