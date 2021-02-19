9861e47bec1b7f4a18797cf78cb3d11f74d756fe
i_liusiyuan <i_liusiyuan@cvte.com>
2021-02-19 10:26:50 +0800

    Table组件demo更新

---
+++ demo/Table/Table1.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Table } from "../../src";
+
+const App = () => {
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Tags",
+      key: "tags",
+      dataIndex: "tags",
+    },
+  ];
+
+  const data = [
+    {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      tags: ["nice", "developer"],
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      tags: ["loser"],
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      age: 32,
+      address: "Sidney No. 1 Lake Park",
+      tags: ["cool", "teacher"],
+    },
+  ];
+  return (
+    <div style={{ width: "500px" }}>
+      <Table columns={columns} dataSource={data}></Table>
+    </div>
+  );
+};
+
+export default App;
