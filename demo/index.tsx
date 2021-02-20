78715b6713d520c58f780ba8ce63d2228c9cf87b
付金祥 <fujinxiang@cvte.com>
2021-02-20 10:48:16 +0800

    去掉不需要的文件

--- demo/index.tsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom";
-import React from "react";
-// import App from "./other";
-import App from "./Tooltip/Tooltip1";
-
-ReactDOM.render(<App />, document.querySelector("#container"));
